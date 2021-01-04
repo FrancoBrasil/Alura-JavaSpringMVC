e7490aeeee15e76968df3b1b979d2dd3ca76aefa
FrancoBrasil <fcbs.web@gmail.com>
2021-01-04 14:44:20 -0300

    Paginação e Cache

--- src/main/java/br/com/alura/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mudi/WebSecurityConfig.java
@@ -1,36 +1,61 @@
 package br.com.alura.mudi;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private javax.sql.DataSource dataSource;
-
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and()
-				.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll())
-				//.logout(logout -> logout.logoutUrl("/logout"));
-				.logout(logout -> logout.logoutUrl("/logout"));
-				//.logout(logout -> logout.logoutUrl("/logout"));
+		http
+		.authorizeRequests()
+		.antMatchers("/home/**")
+			.permitAll()
+		.anyRequest()
+			.authenticated()
+		.and()
+		.formLogin(form -> form
+            .loginPage("/login")
+            .defaultSuccessUrl("/usuario/pedido", true)
+            .permitAll()
+        )
+		.logout(logout -> {
+			logout.logoutUrl("/logout")
+				.logoutSuccessUrl("/home");
+		});
 	}
-
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
-
-		auth.jdbcAuthentication()
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		auth
+			.jdbcAuthentication()
 			.dataSource(dataSource)
-			.passwordEncoder(enconder);
-
+			.passwordEncoder(encoder);
+		
+//		UserDetails user =
+//				 User.builder()
+//					.username("maria")
+//					.password(encoder.encode("maria"))
+//					.roles("ADM")
+//					.build();
 	}
+	
 }
